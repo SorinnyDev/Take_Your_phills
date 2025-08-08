d1520050913343239c76484ad08dd942f6c7f009
SorinnyDev <cjkklled@gmail.com>
2025-08-09 01:30:53 +0900

    스위치 값 변경되게 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,228 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        fontFamily: 'Pretendard',
+        primaryColor: Color(0xFF1C2D5A),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ReminderListScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class ReminderListScreen extends StatelessWidget {
+  void _goToDetail(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_) => ReminderDetailScreen()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Container(
+            height: 180,
+            decoration: BoxDecoration(
+              color: Color(0xFF1C2D5A),
+              borderRadius: BorderRadius.vertical(bottom: Radius.circular(30)),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('약 챙겨 먹었니', style: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold)),
+                  SizedBox(height: 20),
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 2,
+                      childAspectRatio: 1.3,
+                      crossAxisSpacing: 12,
+                      mainAxisSpacing: 12,
+                      children: List.generate(6, (index) => ReminderCard(onTap: () => _goToDetail(context))),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    );
+  }
+}
+
+class ReminderCard extends StatefulWidget {
+  final VoidCallback onTap;
+
+  ReminderCard({required this.onTap});
+
+  @override
+  State<ReminderCard> createState() => _ReminderCardState();
+}
+
+class _ReminderCardState extends State<ReminderCard> {
+  bool isEnabled = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: widget.onTap,
+      child: Container(
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 6)],
+        ),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text('종합비타민', style: TextStyle(fontWeight: FontWeight.bold)),
+                Spacer(),
+                Switch(
+                  value: isEnabled, 
+                  onChanged: (value) {
+                    setState(() {
+                      isEnabled = value;
+                    });
+                  }
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Text('10:10 AM', style: TextStyle(fontSize: 16)),
+            Text('매 12시간', style: TextStyle(color: Colors.grey[600]))
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ReminderDetailScreen extends StatefulWidget {
+  @override
+  State<ReminderDetailScreen> createState() => _ReminderDetailScreenState();
+}
+
+class _ReminderDetailScreenState extends State<ReminderDetailScreen> {
+  String amPm = 'AM';
+  int hour = 10;
+  int minute = 10;
+  int repeatHour = 10;
+  int repeatMinute = 10;
+  bool isEnabled = true;
+  TextEditingController titleController = TextEditingController();
+
+  List<int> hourList = List.generate(12, (i) => i + 1);
+  List<int> minuteList = List.generate(60, (i) => i);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Text('알림 생성', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            ),
+            SizedBox(height: 24),
+            Text('알림 시간', style: TextStyle(fontWeight: FontWeight.bold)),
+            Row(
+              children: [
+                DropdownButton<String>(
+                  value: amPm,
+                  items: ['AM', 'PM'].map((val) => DropdownMenuItem(value: val, child: Text(val))).toList(),
+                  onChanged: (val) => setState(() => amPm = val!),
+                ),
+                SizedBox(width: 12),
+                DropdownButton<int>(
+                  value: hour,
+                  items: hourList.map((val) => DropdownMenuItem(value: val, child: Text(val.toString()))).toList(),
+                  onChanged: (val) => setState(() => hour = val!),
+                ),
+                SizedBox(width: 12),
+                DropdownButton<int>(
+                  value: minute,
+                  items: minuteList.map((val) => DropdownMenuItem(value: val, child: Text(val.toString().padLeft(2, '0')))).toList(),
+                  onChanged: (val) => setState(() => minute = val!),
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+            Text('반복 간격', style: TextStyle(fontWeight: FontWeight.bold)),
+            Row(
+              children: [
+                DropdownButton<int>(
+                  value: repeatHour,
+                  items: hourList.map((val) => DropdownMenuItem(value: val, child: Text(val.toString()))).toList(),
+                  onChanged: (val) => setState(() => repeatHour = val!),
+                ),
+                SizedBox(width: 12),
+                DropdownButton<int>(
+                  value: repeatMinute,
+                  items: minuteList.map((val) => DropdownMenuItem(value: val, child: Text(val.toString().padLeft(2, '0')))).toList(),
+                  onChanged: (val) => setState(() => repeatMinute = val!),
+                ),
+                SizedBox(width: 8),
+                Text('마다')
+              ],
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('알림 활성화', style: TextStyle(fontWeight: FontWeight.bold)),
+                Switch(value: isEnabled, onChanged: (val) => setState(() => isEnabled = val)),
+              ],
+            ),
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(hintText: '알림 제목', border: OutlineInputBorder(borderRadius: BorderRadius.circular(8))),
+            ),
+            SizedBox(height: 24),
+            Row(
+              children: [
+                Expanded(
+                  child: OutlinedButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text('취소'),
+                  ),
+                ),
+                SizedBox(width: 12),
+                Expanded(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(backgroundColor: Color(0xFF1C2D5A)),
+                    onPressed: () {},
+                    child: Text('저장'),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
